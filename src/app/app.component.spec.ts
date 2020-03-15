8507c88cf74ac976a21ce36797f7a2e9ba13b886
viettranisnottaken <tranxuanviet1997@gmail.com>
2020-03-15 16:13:56 +0700

    first init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-heros-v9'`, () => {
+  it(`should have as title 'Tour of Heroes v9'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-heros-v9');
+    expect(app.title).toEqual('Tour of Heroes v9');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-heros-v9';
+  title = 'Tour of Heroes v9';
 }
